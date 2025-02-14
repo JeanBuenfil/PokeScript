6ec15540de2090655a3895c4d0e3c0d9a3d0faf4
Jean Buenfil <jeanbuenfil.jb@gmail.com>
2025-02-13 22:08:41 -0600

    añadido el script

---
+++ pokescript.sh
@@ -0,0 +1,25 @@
+if test $1 -n
+then
+salida = curl https://pokeapi.co/api/v2/pokemon/$1
+if salida -n
+    then
+    nombre = $(echo $salida | jq .name)
+    orden = $(echo $salida | jq .order)
+    id = $(echo $salida | jq .id)
+    peso = $(echo $salida | jq .weight)
+    altura $(echo $salida | jq .height)
+
+    echo "
+    $nombre (NO. $orden)
+    Id = $id
+    Weight = $peso
+    Height = $altura
+    "
+
+    echo "$id,$nombre,$peso,$altura,$orden" >> pokemon.csv
+    else
+    echo "Ese pokemon no existe"
+    fi
+else
+echo "No se pasaron argumentos, inténtelo de nuevo escribiendo el nombre de un pokemon" 
+fi
